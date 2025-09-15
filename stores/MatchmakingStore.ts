4f352a044c048cadf04ee0a4201044bf1d255616
Luke Policinski <Luke@LukePOLO.com>
2025-09-15 18:02:44 -0400

    bug: parse floats

--- stores/MatchmakingStore.ts
+++ stores/MatchmakingStore.ts
@@ -411,21 +411,10 @@ export const useMatchmakingStore = defineStore("matchmaking", () => {
           return true;
         }
 
-        console.info({
-          region: region.value,
-          isLan: region.is_lan,
-          isLanResult: regionLatency?.isLan,
-          regionLatency: regionLatency?.latency,
-          maxAcceptableLatency: useApplicationSettingsStore().maxAcceptableLatency,
-          defaulted: useApplicationSettingsStore().maxAcceptableLatency || 100,
-          check: regionLatency?.latency >
-            (useApplicationSettingsStore().maxAcceptableLatency || 100),
-        });
-        
         if (
           regionLatency &&
-          regionLatency?.latency >
-            (useApplicationSettingsStore().maxAcceptableLatency || 100)
+          parseFloat(regionLatency?.latency) >
+            parseFloat(useApplicationSettingsStore().maxAcceptableLatency || "100")
         ) {
           return false;
         }
